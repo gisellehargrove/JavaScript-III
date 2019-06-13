a6ceb930089bec7167277e7cd350ce7c26b45bea
gisellehargrove <gisellehargrove@gmail.com>
2019-06-13 14:31:39 -0400

    Update prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -63,7 +63,6 @@ Humanoid.prototype.greet = function() {
   return `${this.name} offeres a greeting in ${this.language}`
 }
 
-
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -135,7 +134,22 @@ Humanoid.prototype.greet = function() {
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
+
   // Stretch task:
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  function Hero(attrs) {
+    Humanoid.call(this, attrs)
+    this.attack = function() {
+
+    }
+
+  }
+
+  function Villain(attrs) {
+    Humanoid.call(this, attrs)
+
+
+  }
